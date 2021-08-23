32de758c646286c0d6ed442a55bb022842b8017f
tales-zup <tales.araujo@zup.com.br>
2021-08-23 12:09:57 -0300

    correcao repo

--- src/main/java/com/zup/desafio/repositorio/AlunoRepositorio.java
+++
@@ -1,6 +0,0 @@
-package com.zup.desafio.repositorio;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface AlunoRepositorio<Aluno> extends CrudRepository<Aluno, Long> {
-}
---
+++ src/main/java/com/zup/desafio/repositorio/AutorRepositorio.java
@@ -0,0 +1,7 @@
+package com.zup.desafio.repositorio;
+
+import com.zup.desafio.modelo.Autor;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AutorRepositorio extends CrudRepository<Autor, Long> {
+}
