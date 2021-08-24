207afd67d72d22260c7003da9c4a795d63f4c036
tales-zup <tales.araujo@zup.com.br>
2021-08-24 08:56:06 -0300

    melhorando resposta de erro

--- src/main/java/com/zup/desafio/controller/AutorController.java
+++ src/main/java/com/zup/desafio/controller/AutorController.java
@@ -4,7 +4,6 @@ import com.zup.desafio.modelo.Autor;
 import com.zup.desafio.repositorio.AutorRepositorio;
 import com.zup.desafio.web.AutorRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
