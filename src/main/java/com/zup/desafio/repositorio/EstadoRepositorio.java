9ca5ebcaec7c4a690c32123cb34cdb75417f9dde
tales-zup <tales.araujo@zup.com.br>
2021-08-25 13:57:43 -0300

    estado repositorio

---
+++ src/main/java/com/zup/desafio/repositorio/EstadoRepositorio.java
@@ -0,0 +1,7 @@
+package com.zup.desafio.repositorio;
+
+import com.zup.desafio.modelo.Estado;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EstadoRepositorio extends CrudRepository<Estado, Long> {
+}
