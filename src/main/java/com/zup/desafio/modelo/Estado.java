8f97a5cdcd635641a4e6cb9823ff69bb20216913
tales-zup <tales.araujo@zup.com.br>
2021-08-25 13:55:29 -0300

    entidade estado

---
+++ src/main/java/com/zup/desafio/modelo/Estado.java
@@ -0,0 +1,19 @@
+package com.zup.desafio.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Estado {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    public Estado() {
+    }
+}
