46a2d08460a0154565e3090df20524b98c4356b7
tales-zup <tales.araujo@zup.com.br>
2021-08-25 13:39:28 -0300

    pais repository

---
+++ src/main/java/com/zup/desafio/repositorio/PaisRepository.java
@@ -0,0 +1,7 @@
+package com.zup.desafio.repositorio;
+
+import com.zup.desafio.modelo.Pais;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PaisRepository extends CrudRepository<Pais, Long> {
+}
