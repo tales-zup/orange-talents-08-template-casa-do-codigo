60449a52f3ac9c884d5d2841a7416118129415f0
tales-zup <tales.araujo@zup.com.br>
2021-08-25 13:36:32 -0300

    entidade país

---
+++ src/main/java/com/zup/desafio/modelo/Pais.java
@@ -0,0 +1,25 @@
+package com.zup.desafio.modelo;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Pais {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @Deprecated
+    public Pais() {
+    }
+
+    public Pais(String nome) {
+        this.nome = nome;
+    }
+}
