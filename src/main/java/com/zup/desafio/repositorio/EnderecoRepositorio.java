11fc7b4f2df40c95e5df3224853d86490a30a8c0
tales-zup <tales.araujo@zup.com.br>
2021-08-25 16:19:31 -0300

    endereco repositorio

---
+++ src/main/java/com/zup/desafio/repositorio/EnderecoRepositorio.java
@@ -0,0 +1,7 @@
+package com.zup.desafio.repositorio;
+
+import com.zup.desafio.modelo.Endereco;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EnderecoRepositorio extends CrudRepository<Endereco, Long> {
+}
